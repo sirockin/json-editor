bbd5ce0862d275ffc5341bcdbd681f2b8a29cc72
Dave Sirockin <dave@flow-marine.com>
2019-08-28 09:20:53 +0200

    temporary additons to facilitate testing

--- tests/codeceptjs/editors/button_test.js
+++ tests/codeceptjs/editors/button_test.js
@@ -10,7 +10,7 @@ Scenario('should work with button editor callbacks', async (I) => {
 });
 
 // TODO - remove @optional once we have worked out why test failing
-Scenario('should work with button editor with option validated = true @optional', async (I) => {
+Scenario('should work with button editor with option validated = true @failing_button', async (I) => {
   I.amOnPage('button-callbacks.html');
   I.seeElement('[data-schemapath="root.button2"] button');
   assert.equal(await I.grabAttributeFrom('[data-schemapath="root.button2"] button', 'disabled'), 'true');
@@ -18,6 +18,7 @@ Scenario('should work with button editor with option validated = true @optional'
   I.seeElement('[data-schemapath="root.textinput"] input');
   await I.fillField('[name="root[textinput]"]', 'Hello World');
   // Test fails on next line and I have no idea why - it works when I carry out these steps manually
+  I.click('[data-schemapath="root.button1"] button'); // Added on advice of @pm65
   assert.equal(await I.grabAttributeFrom('[data-schemapath="root.button2"] button', 'disabled'), 'false');
   I.seeElement('[data-schemapath="root.button2"] button');
   I.click('[data-schemapath="root.button2"] button');
